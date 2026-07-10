416e6076cf9326cbae4c76a078e85c8d98dfa108
Ryan Felone <rfelone@hyperice.com>
2026-07-10 17:56:16 -0400

    update sheet key

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 export const DEFAULT_SHEET_ID: string =
   (import.meta.env.VITE_GOOGLE_SHEET_ID as string | undefined) ?? "";
 
-export const LS_SHEET_KEY = "picolo_sheet_id";
+export const LS_SHEET_KEY = "Sheet1";
 export const MIN_PLAYERS = 2;
 export const MAX_PLAYERS = 10;
